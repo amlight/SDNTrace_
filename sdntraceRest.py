683c41c89643b968012aab2a0d0b9b4f98546136
sdn <sdn@onos1-drake-exp.lab.sdn.amlight.net>
2016-03-11 13:02:21 -0500

    REST interface added

---
+++ sdntraceRest.py
@@ -0,0 +1,70 @@
+import json
+import sdntrace
+from webob import Response
+from ryu.app.wsgi import ControllerBase, WSGIApplication, route
+
+
+sdntrace_instance_name = 'sdntrace_api_app'
+
+
+class SDNTraceRest(sdntrace.SDNTrace):
+
+    _CONTEXTS = {'wsgi': WSGIApplication}
+
+    def __init__(self, *args, **kwargs):
+        super(SDNTraceRest, self).__init__(*args, **kwargs)
+        wsgi = kwargs['wsgi']
+        wsgi.register(SDNTraceController, {sdntrace_instance_name: self})
+
+
+class SDNTraceController(ControllerBase):
+
+    def __init__(self, req, link, data, **config):
+        super(SDNTraceController, self).__init__(req, link, data, **config)
+        self.sdntrace_app = data[sdntrace_instance_name]
+
+    @route('sdntrace', '/sdntrace/switches', methods=['GET'])
+    def print_switches(self, req, **kwargs):
+        return self._switches(req, **kwargs)
+
+    @route('sdntrace', '/sdntrace/switches/{dpid}/ports', methods=['GET'])
+    def print_switch_ports(self, req, **kwargs):
+        return self._switch_ports(req, **kwargs)
+
+    @route('sdntrace', '/sdntrace/switches/{dpid}/neighbors', methods=['GET'])
+    def print_switch_neighbors(self, req, **kwargs):
+        return self._switch_neighbors(req, **kwargs)
+
+    @route('sdntrace', '/sdntrace/trace', methods=['PUT'])
+    def run_trace(self, req, **kwargs):
+        return self._trace(req, **kwargs)
+
+    def _switches(self, req, **kwargs):
+        sws = [node.name for node in self.sdntrace_app.node_list]
+        body = json.dumps(sws)
+        return Response(content_type='application/json', body=body)
+
+    def _switch_ports(self, req, **kwargs):
+        dpid = kwargs['dpid']
+        for node in self.sdntrace_app.node_list:
+            if node.name == dpid:
+                ports = node.ports
+        body = json.dumps(ports)
+        return Response(content_type='application/json', body=body)
+
+    def _switch_neighbors(self, req, **kwargs):
+        dpid = kwargs['dpid']
+        for node in self.sdntrace_app.node_list:
+            if node.name == dpid:
+                neighbors = [node.name for node in node.adjenceciesList]
+        body = json.dumps(neighbors)
+        return Response(content_type='application/json', body=body)
+
+    def _trace(self, req, **kwargs):
+        nodes_app = self.sdntrace_app
+        new_entry = eval(req.body)
+        # Process trace
+        result = nodes_app.process_trace_req(new_entry)
+        print result
+        body = json.dumps(1)
+        return Response(content_type='application/json', body=body)
---
+++ trace_pkt.py
@@ -0,0 +1,105 @@
+from ryu.lib.packet import ethernet, vlan, packet, ipv4, tcp
+
+
+def prepare_switch(switch, dpid, in_port):
+    for idx in switch:
+        if idx == 'dpid':
+            dpid = switch[idx]
+        elif idx == 'in_port':
+            in_port = switch[idx]
+    return dpid, in_port
+
+
+def prepare_ethernet(eth, dl_src, dl_dst, dl_vlan, dl_type):
+    for idx in eth:
+        if idx == 'dl_src':
+            dl_src = eth[idx]
+        elif idx == 'dl_dst':
+            dl_dst = eth[idx]
+        elif idx == 'dl_vlan':
+            dl_vlan = eth[idx]
+        elif idx == 'dl_type':
+            dl_type = eth[idx]
+    return dl_src, dl_dst, dl_vlan, dl_type
+
+
+def prepare_ip(ip, nw_src, nw_dst, nw_tos):
+    for idx in ip:
+        if idx == 'nw_src':
+            nw_src = ip[idx]
+        elif idx == 'nw_dst':
+            nw_dst = ip[idx]
+        elif idx == 'nw_tos':
+            nw_tos = ip[idx]
+    return nw_src, nw_dst, nw_tos
+
+
+def prepare_tp(tp, tp_src, tp_dst):
+    for idx in tp:
+        if idx == 'tp_src':
+            tp_src = tp[idx]
+        elif idx == 'tp_dst':
+            tp_dst = tp[idx]
+    return tp_src, tp_dst
+
+
+def generate_trace_pkt(entries, color):
+    '''
+        Receives the REST/PUT to generate a PacketOut
+        data needs to be serialized
+        template_trace.json is an example
+    '''
+    trace = {}
+    switch = {}
+    eth = {}
+    ip = {}
+    tp = {}
+
+    dpid, in_port = 0, 65532
+    dl_src, dl_dst = "ca:fe:ca:fe:00:00", "ca:fe:ca:fe:ca:fe"
+    dl_vlan, dl_type = 0, 2048
+    nw_src, nw_dst, nw_tos = '127.0.0.1', '127.0.0.1', 0
+    tp_src, tp_dst = 1, 1
+
+    try:
+        trace = entries['trace']
+        switch = trace['switch']
+        eth = trace['eth']
+        ip = trace['ip']
+        tp = trace['tp']
+    except:
+        pass
+
+    if len(switch) > 0:
+        dpid, in_port = prepare_switch(switch, dpid, in_port)
+
+    if len(eth) > 0:
+        dl_src, dl_dst, dl_vlan, dl_type = prepare_ethernet(eth, dl_src, dl_dst,
+                                                            dl_vlan, dl_type)
+    if len(ip) > 0:
+        nw_src, nw_dst, nw_tos = prepare_ip(ip, nw_src, nw_dst, nw_tos)
+
+    if len(tp) > 0:
+        tp_src, tp_dst = prepare_tp(tp, tp_src, tp_dst)
+
+    pkt = packet.Packet()
+
+    if dl_vlan is 0:
+        eth_pkt = ethernet.ethernet(dst=dl_dst, src=dl_src, ethertype=dl_type)
+        pkt.add_protocol(eth_pkt)
+    else:
+        eth_pkt = ethernet.ethernet(dst=dl_dst, src=dl_src, ethertype=33024)
+        vlan_pkt = vlan.vlan(vid=dl_vlan, ethertype=dl_type, pcp=int(color, 2))
+        pkt.add_protocol(eth_pkt)
+        pkt.add_protocol(vlan_pkt)
+
+    if dl_type is 2048:
+        ip_pkt = ipv4.ipv4(dst=str(nw_dst), src=str(nw_src), tos=nw_tos,
+                           proto=6)
+        pkt.add_protocol(ip_pkt)
+        tp_pkt = tcp.tcp(dst_port=tp_dst, src_port=tp_src)
+        pkt.add_protocol(tp_pkt)
+
+    pkt.serialize()
+    print pkt
+    return in_port, pkt
